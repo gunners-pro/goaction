75f2f6a324e8252cda31658247dbf2cb6f07aa02
Fabricyo Barreto <bass.barreto@gmail.com>
2021-02-28 15:21:06 -0300

    Notification/Audio: Foi adicionado essas funcionalidades quando surgir um novo desafio, tocará um audio e o usuário será notificado.

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,6 @@
-import { createContext, ReactNode, useState } from 'react';
+import {
+  createContext, ReactNode, useEffect, useState,
+} from 'react';
 
 import challenges from '../../challenges.json';
 
@@ -36,6 +38,10 @@ export default function ChallengesProvider({ children }: ChallengesProviderProps
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
+  useEffect(() => {
+    Notification.requestPermission();
+  }, []);
+
   function levelUp() {
     setLevel(level + 1);
   }
@@ -44,6 +50,14 @@ export default function ChallengesProvider({ children }: ChallengesProviderProps
     const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
     const challenge = challenges[randomChallengeIndex];
     setActiveChallenge(challenge);
+
+    new Audio('/notification.mp3').play();
+
+    if (Notification.permission === 'granted') {
+      new Notification('Novo Desafio !', {
+        body: `Valendo ${challenge.amount}xp !`,
+      });
+    }
   }
 
   function resetChallenge() {
